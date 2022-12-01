f2de9fa8fee260d5fd4b92be28d8e96a2fd7c197
Erkan <12980@coderacademy.edu.au>
2022-12-01 18:44:48 +1100

    completed mini max challenge

---
+++ miniMaxSum/script.js
@@ -0,0 +1,20 @@
+function miniMAxSum(arr) {
+   let max = arr[0]
+   let min = arr[0]
+   let sum = 0
+   for (let i = 0; i < 5; i++){
+        if (max < arr[i]) {
+            max = arr[i]
+        }
+        if (min > arr [i]) {
+            min = arr[i]
+        }
+        sum += arr[i]
+   }
+   let maxSum = sum - min
+   let minSum = sum - max
+   console.log(minSum + " " + maxSum)
+    
+}
+
+miniMAxSum([1, 2, 3, 4, 5])
